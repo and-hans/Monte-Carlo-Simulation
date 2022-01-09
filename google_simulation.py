6fd13e26fa4c7a05710b48870a917fab2881a1c9
Andrew Hansraj <hansrajandrew@gmail.com>
2022-01-09 01:34:18 -0500

    finished off calculations

--- google_simulation.py
+++ google_simulation.py
@@ -1,3 +1,67 @@
 import pandas as pd
+from math import log
+from math import sqrt
+from math import exp
+from random import uniform
+from statistics import NormalDist
 
-GOOG = pd.read_csv("GOOGL.csv", index_col=0)
+
+GOOGL = pd.read_csv("GOOGL.csv", index_col=0)
+
+closing = []
+
+for stock in GOOGL["Adj Close"]:
+    closing.append(stock)
+
+
+def periodicDailyReturn():
+    periodicDailyReturn = []
+    for i in range(0, len(closing)):
+        if (i+1) != 253:
+            periodicDailyReturn.append(log(closing[i+1]/closing[i]))
+    return periodicDailyReturn
+
+
+averagePeriodicDailyReturn = sum(periodicDailyReturn())/len(periodicDailyReturn())  # noqa: E501
+
+
+def variance():
+    mean = 0
+    for i in range(0, len(periodicDailyReturn())):
+        mean += (periodicDailyReturn()[i] - averagePeriodicDailyReturn)**2
+    return mean/len(periodicDailyReturn())
+
+
+drift = averagePeriodicDailyReturn - (variance()/2)
+
+standard_deviation = sqrt(variance())
+
+
+def randomValues():
+    randList = []
+    for i in range(0, 253):
+        rand = uniform(0, 1)
+        randList.append(standard_deviation * NormalDist().inv_cdf(rand))
+    return randList
+
+
+def predictedPrices():
+    predictedPricesList = []
+    for i in range(len(periodicDailyReturn())):
+        predictedPricesList.append(closing[i] * exp(drift+randomValues()[i]))
+    return predictedPricesList
+
+
+def percentDifference():
+    differenceList = []
+    for i in range(len(periodicDailyReturn())):
+        numerator = abs(predictedPrices()[i] - closing[i])
+
+        denominator = (predictedPrices()[i] + closing[i])/2
+
+        total = (numerator/denominator)*100
+
+        differenceList.append(round(total, 2))
+    return differenceList
+
+
